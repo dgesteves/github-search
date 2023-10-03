6e454a39b1449557d21fe7fef26569916b874ade
diogoesteves <diogo.esteves@eino.ai>
2023-10-03 08:55:44 +0100

    create basic tests

--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -1,13 +1,7 @@
-import { getGreeting } from '../support/app.po';
-
 describe('github-search', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome github-search');
+    cy.url().should('include', '/finder');
   });
 });
---
+++ src/components/Card/Card.spec.tsx
@@ -0,0 +1,37 @@
+import { render } from '@testing-library/react';
+import Card from './index';
+import { client } from '../../services/client';
+import { ApolloProvider } from '@apollo/client';
+import { vi } from 'vitest';
+import { Repository } from '../../services/api/types';
+import { FavoriteRepository } from '../../types';
+import { BrowserRouter } from 'react-router-dom';
+
+describe('App', () => {
+  const mockRepository = {
+    id: 'test',
+    url: 'test',
+    name: 'test',
+    description: 'test',
+    updatedAt: '2021-10-10',
+    openGraphImageUrl: 'test',
+  };
+  const mockFavoriteRepo = { ...mockRepository, rating: 5 };
+  const mockFavoriteRepos = [mockFavoriteRepo];
+  const mockSetFavoriteRepos = vi.fn();
+
+  it('should render successfully', () => {
+    const { baseElement } = render(
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Card
+            repository={mockRepository as Repository}
+            favoriteRepos={mockFavoriteRepos as FavoriteRepository[]}
+            setFavoriteRepos={mockSetFavoriteRepos}
+          />
+        </BrowserRouter>
+      </ApolloProvider>
+    );
+    expect(baseElement).toBeTruthy();
+  });
+});
